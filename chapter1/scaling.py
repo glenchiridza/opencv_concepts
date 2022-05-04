7959860bff8fa2f55eca1dcbd3b484be084f2a54
glenchiridza <glenchiridz@gmail.com>
2022-05-04 19:53:03 +0200

    image scaling

--- chapter1/scaling.py
+++ chapter1/scaling.py
@@ -2,6 +2,8 @@ import cv2
 
 img = cv2.imread("../assets/person.png")
 
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow("Gray Image",imgGray)
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 0)
+cv2.imshow("Gray Image", imgGray)
+cv2.imshow("Blur Image", imgBlur)
 cv2.waitKey(0)

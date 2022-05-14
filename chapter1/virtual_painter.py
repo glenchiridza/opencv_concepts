25bfe1836ef71c1bcbfe99540159bff3240694de
glenchiridza <glenchiridz@gmail.com>
2022-05-14 15:26:56 +0200

    findcolors

--- chapter1/virtual_painter.py
+++ chapter1/virtual_painter.py
@@ -7,7 +7,15 @@ vid = cv2.VideoCapture(0)
 
 vid.set(3,640)
 vid.set(4,480)
-vid.set(10,100)
+vid.set(10,140)
+
+
+def findColor(img):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+
 
 while True:
     success,img = vid.read()
